c5d92fe23d8c17fc9928cf098af9faae04729cc8
yapiko <bloomingjewels7021@gmail.com>
2022-11-19 09:19:44 +0900

    kokeshicafe17

--- phplogin/staff_top.php
+++ phplogin/staff_top.php
@@ -5,7 +5,7 @@
         <title>kokeshicafe</title>
     </head>
     <body>
-        <p>スタッフ管理トップメニュー</p>
+        <p>スタッフ管理トップメニュー<br>
             <a href="../phpside/staff_list.php">スタッフ管理</a><br>
             <a href="../phpside/ichiran.php">お問い合わせ一覧</a><br>
             <a href="../phpside/kensaku.html">お問い合わせ検索</a><br>
